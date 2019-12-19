52568d37804395e3b6bfcdfa8fc881f391cad199
Himashi Hettege Dona <himashi99@hotmail.com>
2019-12-18 22:49:25 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,82 @@
+// const containers = document.querySelectorAll('li');
+// const sortable = new Sortable.default(containers, {
+//   draggable: 'div',
+//   appendTo: 'body',
+//   // mirror: {
+//   //   constrainDimensions: false,
+//   //   cursorOffsetX: 10,
+//   //   cursorOffsetY: 10,
+//   //   xAxis: false
+//   // },
+// });
+
+// sortable.on('drag:start', (event) => {
+//   event.originalSource.classList.add('original-source');
+// });
+
+// sortable.on('drag:stop', (event) => {
+//   event.originalSource.classList.remove('original-source');
+// });
+
+
+
+const containers = document.querySelectorAll('li.swap');
+const swappable = new Swappable.default(containers, {
+  draggable: 'div',
+  mirror: {
+    appendTo: containers,
+    constrainDimensions: true,
+  },
+});
+
+// swappable.on('swappable:start', () => console.log('swappable:start'));
+// swappable.on('swappable:swapped', () => console.log('swappable:swapped'));
+// swappable.on('swappable:stop', () => console.log('swappable:stop'));
+
+
+
+
+const containers1 = document.querySelectorAll('ul.yo')
+const draggable = new Draggable.default(containers1, {
+  draggable: 'li'
+});
+
+draggable.on('drag:start', () => console.log('drag:start'));
+draggable.on('drag:move', () => console.log('drag:move'));
+draggable.on('drag:stop', () => console.log('drag:stop'));
+
+
+
+
+// const containerSelector = '.capabilities';
+// const containers11 = document.querySelectorAll(containerSelector);
+
+// const swappable11 = new Swappable.default(containers11, {
+//   draggable: '.Block--isDraggable',
+//   mirror: { 
+//     appendTo: containerSelector,
+//     constrainDimensions: true,
+//   },
+//   plugins: [Plugins.ResizeMirror],
+// });
+
+
+
+
+
+// const containers2 = document.querySelectorAll('capabilities')
+const gridContainer = document.querySelectorAll('.capabilities');
+
+const swappable1 = new Swappable.default(gridContainer, {
+  draggable: '.isDraggable',
+  mirror: {
+    appendTo: gridContainer,
+    constrainDimensions: true,
+  },
+  plugins: [Plugins.ResizeMirror],
+});
+
+
+swappable1.on('swappable:start', (a) => console.log('swappable:start', a));
+swappable1.on('swappable:swapped', (a) => console.log('swappable:swapped', a));
+swappable1.on('swappable:stop', () => console.log('swappable:stop'));
