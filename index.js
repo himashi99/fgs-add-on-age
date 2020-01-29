66bc3c1f9a2955d367753b79447eb525a70e4247
Himashi Hettege Dona <himashi99@hotmail.com>
2020-01-29 14:15:33 -0500

    Added temp issues to drag and drop for first image

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-// Ideas to Market Graphic
-const ideasMarketGrid = document.querySelectorAll('div.ideas-to-market-grid');
-const swappableIdeas = new Swappable.default(ideasMarketGrid, {
-  draggable: '.ideas-to-market-drag',
-  mirror: {
-    // appendTo: ideasMarketGrid,
-    constrainDimensions: true,
-  },
-});
+// // Ideas to Market Graphic
 
+const containerSelector2 = '#GridLayout2 .BlockLayout2';
+const containers2 = document.querySelectorAll(containerSelector2);
+
+  const swappablee = new Draggable.Swappable(containers2, {
+      draggable: '.Block--isDraggable2',
+      mirror: {
+        appendTo: containers2,
+        constrainDimensions: true,
+        cursorOffsetX: 0,
+        cursorOffsetY: 0,
+      },
+      // defaultOptions: {"draggable": '.Block--isDraggable2', "handle": null, "delay": 100, "placedTimeout": 800,},
+
+      // plugins: [Plugins.ResizeMirror],
+    });
+
+    // swappablee.on('drag:start', (e) => {
+    //   console.log('drag:start')
+    //   e.mirror.style.backgroundColor = '#6CCACD';
+    // })
+    // swappable2.on('drag:start', (a) => {
+    //   console.log('drag:start');
+    //   a.mirror.style.backgroundColor = "purple";
+    //   // e.originalSource.style.backgroundColor='blue';
+    //   // a.mirror.style.backgroundColor = '#6CCACD';
+    // })
+  
 // About Us Section Graphic
 
 // jQuery Drag and Droppable
@@ -113,18 +132,18 @@ case '20':
 // Our Capabilities
 
 const containerSelector = '#GridLayout .BlockLayout';
-  const containers = document.querySelectorAll(containerSelector);
+const containers = document.querySelectorAll(containerSelector);
 
-const swappable = new Draggable.Swappable(containers, {
-    draggable: '.Block--isDraggable',
-    mirror: {
-      appendTo: containerSelector,
-      constrainDimensions: true,
-      cursorOffsetX: 70,
-      cursorOffsetY: 0,
-    },
-    plugins: [Plugins.ResizeMirror],
-  });
+  const swappable = new Draggable.Swappable(containers, {
+      draggable: '.Block--isDraggable',
+      mirror: {
+        appendTo: containerSelector,
+        constrainDimensions: true,
+        cursorOffsetX: 70,
+        cursorOffsetY: 0,
+      },
+      plugins: [Plugins.ResizeMirror],
+    });
 
   // This line of code makes the "About Us" section stop working
   swappable.on('drag:start', (e) => {
@@ -134,37 +153,47 @@ const swappable = new Draggable.Swappable(containers, {
     e.mirror.style.backgroundColor = '#6CCACD';
   })
 
+  swappablee.on('drag:start', (e) => {
+    console.log('drag:start');
+    console.log(e.mirror);
+    // e.originalSource.style.backgroundColor='blue';
+    e.mirror.style.backgroundColor = 'purple';
+  })
 
+// -----------------------------------------------------------------------------------------------
+const containerSelector1 = '#GridLayout1 .BlockLayout1';
+const containers1 = document.querySelectorAll(containerSelector1);
 
+  const swappable1 = new Draggable.Swappable(containers1, {
+      draggable: '.Block--isDraggable1',
+      mirror: {
+        appendTo: containerSelector,
+        // constrainDimensions: true,
+        // cursorOffsetX: 70,
+        // cursorOffsetY: 0,
+      },
+      // plugins: [Plugins.ResizeMirror],
+    });
 
-
-
-
-
-
-
-
-
-
-
-
-
+  // This line of code makes the "About Us" section stop working
+  swappable1.on('drag:start', (e) => {
+    console.log('drag:start');
+    console.log(e.mirror);
+    // e.originalSource.style.backgroundColor='blue';
+    e.mirror.style.transform = 'rotate(45deg)';
+    // document.getElementById("hello").style.transform = 'rotate(45deg)';
+    
+  })
 
  
+  document.getElementById("hello").addEventListener("click", myFunction);
+  console.log(document.getElementById("hello"))
+  function myFunction() {
+    console.log("woohoo");
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  swappable1.on('mirror: img', (e) => {
+    console.log("help me");
+  })
+  
 
